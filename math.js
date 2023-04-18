f201cd8520a9ae44de792306c12b40bd7bef5e26
umokee <hituaev@gmail.com>
2023-04-18 12:13:13 +1000

    lab_work_git
    lab_work_git

---
+++ math.js
@@ -0,0 +1,19 @@
+function sum(a, b)
+{
+    return a + b;
+}
+
+function substract(a, b)
+{
+    return a - b;
+}
+
+function multiply(a, b)
+{
+    return a * b;
+}
+
+function divison(a, b)
+{
+    return a / b;
+}
